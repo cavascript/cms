b4355853f95940c71e704804ac5a26a440da8740
cavascript <cavascript@gmail.com>
2017-06-01 11:21:52 -0500

    Lesson 84 PHP for Beginngers

---
+++ includes/db.php
@@ -0,0 +1,25 @@
+<?php 
+
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_pass'] = "8apo1103";
+$db['db_name'] = "cms";
+
+foreach($db as $key => $value){
+	define(strtoupper($key),$value);
+}
+
+
+
+
+$connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if($connect){
+	echo "DB connected";
+}else{
+	die("Not connected");
+}
+
+
+
+ ?>
